634d706d01083d2d69ba8432d5c79d6cd31e0bd5
dmitriy365377 <tigr365377@gmail.com>
2020-03-12 08:16:48 +0300

    reject

--- test/users.spec.js
+++ test/users.spec.js
@@ -5,13 +5,21 @@ describe('The findUserByEmail funciton', () => {
     it('finds a user by email', (done) => {
         findUserByEmail('dima@gmail.com').then((response) => {
             assert.equal(response.message, 'User found successfully')
-        }) 
+        })
         done()
     })
 
     it('finds a user by email (Using async/await)', async () => {
-        const response = await findUserByEmail('dima@gmail.com') 
+        const response = await findUserByEmail('dima@gmail.com')
         assert.equal(response.message, 'User found successfully')
+    })
+
+    it('rejects with error if user email was not found', () => {
+        return findUserByEmail('x@gmail.com').then(() => {
+            assert.fail('Expected findUserByEmail function to reject.')
+        }, (error) => {
+            assert.equal(error.message, 'User with email:x@gmail.com was not found')
+        })
     })
 })
 
