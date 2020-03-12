422b2fa34de933915159327e02c556a2b19fbf4f
dmitriy365377 <tigr365377@gmail.com>
2020-03-12 08:30:00 +0300

    nyc test

--- test/users.spec.js
+++ test/users.spec.js
@@ -24,4 +24,19 @@ describe('The findUserByEmail funciton', () => {
 })
 
 
+describe('The findUserById function', () => {
+    it('should find a user by id', async () => {
+        const response = await findUserById(1)
+
+        assert.equal(response.message, 'User found successfully')
+    })
+
+    it('should reject if user is not found by id', () => {
+        return findUserById(90).then((response) => {
+            assert.fail("Expected findUserById function to throw")
+        }, (error) => {
+            assert.equal(error.message, "User with id: 90 was not found")
+        })
+    })
+})
 
